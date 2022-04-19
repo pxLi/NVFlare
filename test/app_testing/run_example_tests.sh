b727b9d84fd22b38de80b4ec091bae3b054b0d9a
Peixin Li <pxli@nyu.edu>
2022-04-19 13:27:07 +0800

    Fail app and example tests
    Signed-off-by: Peixin Li <pxli@nyu.edu>

--- test/app_testing/run_example_tests.sh
+++ test/app_testing/run_example_tests.sh
@@ -2,4 +2,4 @@
 
 pip install tensorflow torch torchvision
 
-TF_FORCE_GPU_ALLOW_GROWTH=true python test_runner.py --poc ../../nvflare/poc --n_clients 2 --yaml test_examples.yml --app_path ../../examples --cleanup
+TF_FORCE_GPU_ALLOW_GROWTH=true python test_runner-fail-it.py --poc ../../nvflare/poc --n_clients 2 --yaml test_examples.yml --app_path ../../examples --cleanup
